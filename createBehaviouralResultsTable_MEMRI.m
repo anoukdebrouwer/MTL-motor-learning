c3d935962c7f61e41a5285d23010d6e6a941c8b1
Anouk de Brouwer <ajdebrouwer@gmail.com>
2020-08-27 13:13:46 -0700

    Remove old code

--- createBehaviouralResultsTable_MEMRI.m
+++ createBehaviouralResultsTable_MEMRI.m
@@ -1,9 +1,8 @@
 function createBehaviouralResultsTable_MEMRI(projectPath,saveToPath)
+% createBehaviouralResultsTable_MEMRI
 %
 % Create table with behavioural results of VMR and RL task in MEMRI project,
 % and save table in .csv file.
-%
-% in progress
 
 % MIT License
 % Copyright (c) 2020 Anouk de Brouwer
@@ -50,16 +49,11 @@ RL = array2table(mScore,'RowNames',RL_subj,'VariableNames',RL_varNames);
 
 %% RL: plot group data (Fig 1C)
 
-% iSubj_good = find(strncmp(RL_subj,'S01',3));
-% iSubj_bad = find(strncmp(RL_subj,'S25',3));
-
 % plot average scores across trials of all subjects
 scaledFigure(1.5,1);
 % single curves
 subplot(1,2,1); hold on
 plot(avgLearning_single,'color',[0.5 0.5 0.5])
-%plot(avgScore_single(iSubj_good,:),'g')
-%plot(avgScore_single(iSubj_bad,:),'r')
 plot(mean(avgLearning_single,2),'k','linewidth',2)
 axis square
 xlim([1 20]); ylim([0 100])
@@ -70,8 +64,6 @@ title('Single curves')
 % double curves
 subplot(1,2,2); hold on
 plot(avgLearning_double,'color',[0.5 0.5 0.5])
-%plot(avgScore_double(iSubj_good,:),'g')
-%plot(avgScore_double(iSubj_bad,:),'r')
 plot(mean(avgLearning_double,2),'k','linewidth',2)
 axis square
 xlim([1 20]); ylim([0 100])
@@ -82,34 +74,6 @@ title('Double curves')
 %
 suplabel('RL scores - all subjects','t');
 keyboard
-
-%     % plot average path changes
-%     scaledFigure(0.8,1); hold on
-%     plot(medPathChange_bin','.-','markersize',10,'color',[0.5 0.5 0.5])
-%     plot(medPathChange_bin(iSubj_good,:),'g.-','markersize',10)
-%     plot(medPathChange_bin(iSubj_bad,:),'r.-','markersize',10)
-%     plot(nanmean(medPathChange_bin),'k','linewidth',2)
-%     axis square
-%     xlim([0.5 nBins+0.5])
-%     %set(gca,'xtick',0.5:1:(length(count)+0.5))
-%     %set(gca,'xticklabel',edges)
-%     set(gca,'xtick',1:nBins)
-%     set(gca,'xticklabel',{'0-24','25-49','50-74','75-99'})
-%     set(gca,'ytick',0:25:max(medPathChange_bin(:)))
-%     xlabel('Score [t]'); ylabel('Median path change [t+1]')
-%     title('Effect of score on path change - all subjects')
-%     keyboard
-%
-%     % test if slope of median score over blocks is > 0
-%     [h1,p1,ci1,stats1] = ttest(m_single(:,1));
-%     fprintf('Single curves: mean learning slope = %2.2f, p = %1.3f [%1.3f %1.3f]\n',...
-%         mean(m_single(:,1)),p1,ci1)
-%     [h2,p2,ci2,stats2] = ttest(m_double(:,1));
-%     fprintf('Double curves: mean learning slope = %2.2f, p = %1.3f [%1.3f %1.3f]\n',...
-%         mean(m_double(:,1)),p2,ci2)
-%     [h,p,ci,stats] = ttest(m_all(:,1));
-%     fprintf('All curves: mean learning slope = %2.2f, p = %1.3f [%1.3f %1.3f]\n',...
-%         mean(m_all(:,1)),p,ci)
 
 %% VMR: load experiment details and results file
 
@@ -165,15 +129,11 @@ note = {'early = bin 2-3';'late = bin 9-10';...
 %% VMR: plot group data (Fig 1E)
 
 iMidBin = iBin+3;
-iSubj_good = find(strncmp(VMR_subj,'S01',3)); % example good learner
-iSubj_poor = find(strncmp(VMR_subj,'S25',3)); % example poor learner
 
 % plot average hand angles across all subjects
 scaledFigure(1.5,1);
 subplot(1,4,1:3); hold on
 plot(iMidBin,-handAngle','color',[0.5 0.5 0.5])
-%plot(iMidBin,-handAngle(iSubj_good,:),'g')
-%plot(iMidBin,-handAngle(iSubj_bad,:),'r')
 plot(iMidBin,nanmean(-handAngle,2),'k','linewidth',2)
 xlim([0 nTrials]); ylim([-30 75])
 set(gca,'xtick',0:80:nTrials)
@@ -192,36 +152,8 @@ set(gca,'xtick',[])
 set(gca,'ytick',-30:15:75)
 horline([0 45],'k-')
 title('Report angle')
-suplabel('VMR learning - all subjects','t');
-keyboard
-
-%% Plot distributions of variables (data points with mean+SEM)
-
-scaledFigure(2,1);
-matlabcolors = get(gca,'colororder');
-% RL
-subplot(1,3,1);
-plotMeansWithDataPoints(table2array(RL),[],[],matlabcolors(5,:));
-set(gca,'xticklabel',RL_varNames,'xticklabelrotation',45,'ticklabelinterpreter','none')
-ylim([0 100])
-set(gca,'ytick',0:20:100)
-ylabel('Mean median score')
-title('RL task - scores')
-% VMR angles
-subplot(1,3,2)
-plotMeansWithDataPoints(angles,[],[],matlabcolors(1,:));
-set(gca,'xticklabel',VMR_varNames,'xticklabelrotation',45,'ticklabelinterpreter','none')
-set(gca,'ytick',-45:15:90)
-ylabel('Mean angles')
-title('VMR task - angles')
-% VMR transformed scores
-subplot(1,3,3)
-plotMeansWithDataPoints(table2array(VMR),[],[],matlabcolors(6,:));
-set(gca,'xticklabel',VMR_varNames,'xticklabelrotation',45,'ticklabelinterpreter','none')
-set(gca,'ytick',-45:15:90)
-ylabel('Mean errors')
-title('VMR task - errors')
 %
+suplabel('VMR learning - all subjects','t');
 keyboard
 
 %% Create and save table (.csv file) with scores of both behavioural experiments
